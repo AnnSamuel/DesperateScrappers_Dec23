41007fb1ab0026fe81bcbc781d59cbd7aff19576
navchand2023 <124201475+navchand2023@users.noreply.github.com>
2023-12-17 18:26:19 -0800

    Update BaseClass.java

--- src/test/java/utilities/BaseClass.java
+++ src/test/java/utilities/BaseClass.java
@@ -1,5 +1,6 @@
 package utilities;
 
+import org.testng.annotations.AfterMethod;
 import java.time.Duration;
 
 import org.jsoup.Jsoup;
@@ -7,24 +8,28 @@ import org.jsoup.nodes.Document;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class BaseClass {
-	public static WebDriver driver;
+	//public static WebDriver driver;
+	public static WebDriver driver= new ChromeDriver();
 	static String url = "https://www.tarladalal.com/";
 
-	public static void setUpDriver(String headlessMode) {
-		System.out.println("check headless value" + headlessMode);
-
-		// Enable headless mode based on the parameter value
-		ChromeOptions options = new ChromeOptions();
-		if ((Boolean.parseBoolean(headlessMode) == true)) {
-			options.addArguments("--headless");
-		}
-
-
-		driver = new ChromeDriver(options);// initialize chrome driver
-
+	public static void setUpDriver() {
+		
+//		System.out.println("this is hte browser --->>> " +browser);
+//
+//		if (browser.equalsIgnoreCase("chrome")) {
+//			driver = new ChromeDriver();// initialize chrome driver
+//
+//			// LoggerLoad.info("testing on chrome");
+//		} else if (browser.equalsIgnoreCase("edge")) {
+//			driver = new EdgeDriver();// initialize edge driver
+//
+//			// LoggerLoad.info("testing on edge");
+//		}
+		if(driver==null) {
+		driver = new ChromeDriver();}// initialize chrome driver
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));// implicit wait
 		driver.manage().window().maximize();// maximize the window
 	}
@@ -44,12 +49,13 @@ public class BaseClass {
 	public static WebDriver getDriver() {
 		return driver;
 	}
-
+	
 	public static Document getJsoupDocument() {
 		String htmlSource = driver.getPageSource();
 		Document document = Jsoup.parse(htmlSource);
 		return document;
 	}
+
 
 	public static void refresh() {
 		driver.navigate().refresh();
@@ -68,6 +74,7 @@ public class BaseClass {
 		driver.navigate().back();
 	}
 
+	@AfterMethod
 	public static void tearDown() throws InterruptedException {
 
 		if (driver != null) {
